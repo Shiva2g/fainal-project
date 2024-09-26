bda70f84b173fdc3332bb4b7d919a2480b89d17a
Shiva <shivagerayeli@gmail.com>
2024-09-26 15:38:04 -0600

    add google icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,11 @@ import {Inter} from "next/font/google";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 
-const inter = Inter ({display:"swap", weight:["100","200","300","400","500","600","700","800"], subsets:["latin"]});
+const inter = Inter ({
+  display:"swap",
+  weight:["100","200","300","400","500","700","900"], 
+  subsets:["latin"]
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -15,6 +19,9 @@ export default function RootLayout({ children }) {
   
   return (
     <html lang="en">
+      <head>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+      </head>
       <body className= {inter.className}>
         <Header />
         {children}
