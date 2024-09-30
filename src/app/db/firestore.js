9b33eae6a527a878d7fc5203d7cb095399b4d4e7
Shiva <shivagerayeli@gmail.com>
2024-09-30 10:28:33 -0600

    Fetch DB test

--- src/app/db/firestore.js
+++ src/app/db/firestore.js
@@ -1,16 +1,25 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+// import { collection, getDocs } from "firebase/firestore"; //new
 
-const app = initializeApp({
-
+const firebaseConfig = { 
     apiKey: process.env.FIRESTORE_API_KEY,
     authDomain: process.env.FIRESTORE_AUTH_DOMAIN,
     projectId: process.env.FIRESTORE_PROJECT_ID,
     storageBucket: process.env.FIRESTORE_STORAGE_BUCKET,
     messagingSenderId: process.env.FIRESTORE_MESSAGING_SENDER_ID,
-    appId: process.env.FIRESTORE_APP_ID
-});
+    appId: process.env.FIRESTORE_APP_ID,
+  };
 
+const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 export default db;
+
+{/*export const getCollectionData = async (collectionName) => {  //new
+    const collectionRef = collection(db, collectionName);
+    const snapshot = await getDocs(collectionRef);
+    const data = snapshot.docs.map(doc => doc.data());
+    return data;
+  };
+  */}
--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,19 +1,29 @@
-import db  from "../db/firestore";
-import { getDocs, collection, doc, getFirestore} from 'firebase/firestore';
+
+//"use client";
+ import db  from "../db/firestore";
+ import { getDocs, collection, doc, getFirestore} from 'firebase/firestore';
+
 
 const Products = async () => {
 
-  const snapshot = await getDocs(collection(db, "products"));
-  const documents= snapshot.docs.map(doc => doc.data())
-  return 
-    <div>
-      {documents.map(doc => (
-        <div key={doc.id}>
-            <h2>{doc.name}</h2>
-        </div>
-      ))}
-    </div>
-};
+  const snapshot = await getDocs(collection(db, "laptop"));
+  const documents= snapshot.docs.map(doc => doc.data());
+
+    return (
+      <div>
+        <h1>Product Data</h1>
+        {documents.map(doc => (
+          <div key={doc.id}>
+
+              Name: <h3>{doc.name}</h3>
+            <p>
+              price: {doc.Price}
+            </p>
+          </div>
+        ))}
+      </div>
+      )
+}; 
 
 
 
